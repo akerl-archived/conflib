fc6edae72caa8c80115fced1538b3f07d8874536
Les Aker <me@lesaker.org>
2013-07-10 21:15:11 -0400

    bumped version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='conflib',
-    version='0.1.1',
+    version='0.1.3',
     description='Manage configuration hierarchies',
     author='Les Aker',
     author_email='me@lesaker.org',

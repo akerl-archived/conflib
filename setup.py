dcda92bd17482ca97d8e8d850adbef8ae1507c79
Les Aker <me@lesaker.org>
2014-02-16 09:10:37 -0500

    version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='conflib',
-    version='0.1.4',
+    version='0.1.5',
     description='Manage configuration hierarchies',
     author='Les Aker',
     author_email='me@lesaker.org',
